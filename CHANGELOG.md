366fecade93dff2e3fcab59272f7b0e163ce30e0
Bernard Potocki <bernard.potocki@imanel.org>
2014-06-10 06:30:23 +0200

    Release v1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.1.0
+
+- ssl support
+
 ## 1.0.1
 
 - ensure compatibility with em-websocket
--- lib/websocket/eventmachine/client/version.rb
+++ lib/websocket/eventmachine/client/version.rb
@@ -1,7 +1,7 @@
 module WebSocket
   module EventMachine
     class Client
-      VERSION = '1.0.1'
+      VERSION = '1.1.0'
     end
   end
 end
